9eb6b2b040c4e0157e4d05499eb1d8793a893d12
tshinohara <takuma1201@gmail.com>
2024-02-29 22:04:00 +0900

    Log の biome 対応

--- src/log.ts
+++ src/log.ts
@@ -1,6 +1,6 @@
 import { format } from "date-fns";
 
-export const logger = (type: string, object: any) => {
+export const logger = (type: string, object: unknown) => {
   const now = format(new Date(), "yyyy/MM/dd HH:mm:ss");
   console.log(`[${now}] ${type}: `, object);
 };
